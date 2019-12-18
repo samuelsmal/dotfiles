bcde52c9fb17b41ee4ff445cf5b6463276d11b35
Samuel von Baussnern <samuel.vonbaussnern@gmail.com>
2019-12-18 08:46:46 +0100

    disables gnome software

--- _freyr/migrations/step_4.sh
+++ _freyr/migrations/step_4.sh
@@ -9,6 +9,25 @@ install_scrcpy() {
   cd -
 }
 
+disable_gnome_software () {
+  echo "disablying gnome software"
+  # disables downloading of updates
+  gsettings set org.gnome.software download-updates false
+
+  # these are all the background processes of gnome-software
+  sudo systemctl stop packagekit.service
+  sudo systemctl disable packagekit.service
+  sudo systemctl mask packagekit.service
+  sudo systemctl stop packagekit-offline-update.service
+  sudo systemctl disable packagekit-offline-update.service
+  sudo systemctl mask packgekit-offline-update.service
+  sudo rm -rf /var/cache/PackageKit
+
+  echo "disablying gnome software ... done"
+}
+
+
 migration_step_4() {
   install_scrcpy
+  disable_gnome_software
 }
