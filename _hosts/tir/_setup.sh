92aa0816ee82a525ce974f672f73d02a5bc8aae2
Samuel von Baussnern <samuel.vonbaussnern@gmail.com>
2020-05-10 15:44:55 +0200

    minor fixes

--- _hosts/tir/_setup.sh
+++ _hosts/tir/_setup.sh
@@ -4,4 +4,4 @@ N_STEPS=1
 
 # Mouse back button
 echo "(1/$N_STEPS) appyling mouse back button fix"
-bash ../_system-fixes/logitech_mouse_back_btn/_setup.sh
+bash ../../_system-fixes/logitech_mouse_back_btn/_setup.sh
--- _system-fixes/logitech_mouse_back_btn/_setup.sh
+++ _system-fixes/logitech_mouse_back_btn/_setup.sh
@@ -6,6 +6,6 @@
 
 # Got it from here
 # https://web.archive.org/web/20160604141354/http://forums.logitech.com/t5/Mice-and-Pointing-Devices/Guide-for-setup-Performance-MX-mouse-on-Linux-with-KDE/td-p/517167
-sudo dnf install --yes xbindkeys
+sudo dnf install -y xbindkeys
 
-ln -s ./xbindkeysrc $HOME/.xbindkeysrc
+ln -s $PWD/xbindkeysrc $HOME/.xbindkeysrc
