934594bcec317a5f44838d3ce4180380de548fa3
Samuel von Baussnern <samuel.vonbaussnern@gmail.com>
2020-05-23 16:21:35 +0200

    adds protonmail bridge

--- _freyr/migrations/step_5.sh
+++ _freyr/migrations/step_5.sh
@@ -2,4 +2,5 @@
 
 migration_step_4() {
   bash $FREYR_DIR/_tiddlywiki/_setup.sh
+  bash $FREYR_DIR/_protonmail/_setup.sh
 }
---
+++ _protonmail/_setup.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+BRIDGE_VERSION="1.2.7-1"
+wget https://protonmail.com/download/protonmail-bridge-$BRIDGE_VERSION.x86_64.rpm -P ~/Downloads
+wget https://protonmail.com/download/bridge_pubkey.gpg -P ~/Downloads
+sudo rpm --import bridge_pubkey.gpg
+
+SUCCESSFULL_RETURN_STR="$HOME/Downloads/protonmail-bridge-$BRIDGE_VERSION.x86_64.rpm: digests signatures OK"
+
+if [[ `rpm --checksig "$HOME/Downloads/protonmail-bridge-$BRIDGE_VERSION.x86_64.rpm"` == "$SUCCESSFULL_RETURN_STR" ]]; then
+  #sudo dnf install -y ~/Downloads/protonmail-bridge-$BRIDGE_VERSION.x86_64.rpm
+  rm $HOME/Downloads/protonmail-bridge-$BRIDGE_VERSION.x86_64.rpm
+  rm $HOME/Downloads/bridge_pubkey.gpg
+  echo "protonmail bridge successfully installed, now configure it, go there: https://protonmail.com/bridge/thunderbird#1"
+else
+  echo "WARNING: signatures don't match! aborting installation of protonmail bridge"
+fi
