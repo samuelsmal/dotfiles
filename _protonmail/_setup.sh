cd7b9b50dffef1e596d020b3f9e7027fae82bfae
Samuel von Baussnern <savoba@protonmail.ch>
2020-12-01 09:30:09 +0100

    updates protonmail bridge, and updates update mechanism

--- _protonmail/_setup.sh
+++ _protonmail/_setup.sh
@@ -1,18 +1,22 @@
 #! /bin/bash
 
 # version number + '-1'
-BRIDGE_VERSION="1.4.5-1"
+BRIDGE_VERSION="1.5.2-1"
 
 wget https://protonmail.com/download/protonmail-bridge-$BRIDGE_VERSION.x86_64.rpm -P ~/Downloads
-wget https://protonmail.com/download/bridge_pubkey.gpg -P ~/Downloads
-sudo rpm --import "$HOME/Downloads/bridge_pubkey.gpg"
+
+if ! rpm  -qi gpg-pubkey-\* | grep protonmail > /dev/null 2>&1; then
+  echo "key not installed, downloading and installing bridge pubkey"
+  wget https://protonmail.com/download/bridge_pubkey.gpg -P ~/Downloads
+  sudo rpm --import "$HOME/Downloads/bridge_pubkey.gpg"
+  rm $HOME/Downloads/bridge_pubkey.gpg
+fi
 
 SUCCESSFULL_RETURN_STR="$HOME/Downloads/protonmail-bridge-$BRIDGE_VERSION.x86_64.rpm: digests signatures OK"
 
 if [[ `rpm --checksig "$HOME/Downloads/protonmail-bridge-$BRIDGE_VERSION.x86_64.rpm"` == "$SUCCESSFULL_RETURN_STR" ]]; then
   sudo dnf install -y ~/Downloads/protonmail-bridge-$BRIDGE_VERSION.x86_64.rpm
   rm $HOME/Downloads/protonmail-bridge-$BRIDGE_VERSION.x86_64.rpm
-  rm $HOME/Downloads/bridge_pubkey.gpg
   echo "protonmail bridge successfully installed, now configure it, go there: https://protonmail.com/bridge/thunderbird#1"
 else
   echo "WARNING: signatures don't match! aborting installation of protonmail bridge"
